d3fec5226113bc3c68bcc32244c9b52c030c1dfb
albert.ramirez <albert.ramirez@aloe.com.do>
2021-12-17 14:36:46 -0400

    add services layer and configure solution

--- Library.API/Program.cs
+++ Library.API/Program.cs
@@ -1,4 +1,4 @@
-using Library.Data.Extentions;
+using Library.Services.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.ConfigureDbContext(builder.Configuration);
+builder.Services.ConfigureServices(builder.Configuration);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
---
+++ Library.Services/Extensions/ServiceCollectionExtentions.cs
@@ -0,0 +1,18 @@
+﻿using Library.Data.Extentions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Library.Services.Extensions
+{
+    public static class ServiceCollectionExtentions
+    {
+        public static IServiceCollection ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.ConfigureDbContext(configuration);
+
+            services.ConfigureDataServices();
+
+            return services;
+        }
+    }
+}
