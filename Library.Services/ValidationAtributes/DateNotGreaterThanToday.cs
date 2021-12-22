228ed5d0f503a15ffe9c39171a9e60f01a095d2c
albert.ramirez <albert.ramirez@aloe.com.do>
2021-12-22 15:37:06 -0400

    add date born validation attribute

---
+++ Library.Services/ValidationAtributes/DateNotGreaterThanToday.cs
@@ -0,0 +1,20 @@
+﻿using Library.Services.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.Services.ValidationAtributes
+{
+    internal class DateNotGreaterThanToday : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            AuthorForCreationDTO author = (AuthorForCreationDTO)validationContext.ObjectInstance;
+
+            if (author.DateBorn >= DateTime.Now)
+            {
+                return new ValidationResult(ErrorMessage ?? "The date is invalid.", new[] { "AuthorForCreationDTO" });
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
