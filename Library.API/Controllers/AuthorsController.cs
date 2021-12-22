863f945a7cb97db1bf0c5fe3492d47e4ac21e0c9
albert.ramirez <albert.ramirez@aloe.com.do>
2021-12-22 11:12:21 -0400

    add authors controller and remove example data and controller

---
+++ Library.API/Controllers/AuthorsController.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using Library.Domain.Entities;
+using Library.Services.Contracts;
+using Library.Services.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.API.Controllers
+{
+    [Route("api/autores")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly IService<Author> _service;
+        private readonly IMapper _mapper;
+
+        public AuthorsController(IService<Author> service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+
+        [HttpGet(Name = "GetAuthors")]
+        public async Task<ActionResult<IEnumerable<AuthorForPresentationDTO>>> Get()
+        {
+            IEnumerable<Author> authors = await _service.GetAllAsync();
+            return _mapper.Map<IEnumerable<AuthorForPresentationDTO>>(authors).ToList();
+        }
+
+        [HttpPost(Name = "CreateAuthor")]
+        public async Task<ActionResult<AuthorForPresentationDTO>> Post([FromBody] AuthorForCreationDTO author)
+        {
+            Author authorToCreate = _mapper.Map<Author>(author);
+            Author authorCreated = await _service.AddAsync(authorToCreate);
+
+            return CreatedAtRoute("GetAuthor", new { authorCreated.Id }, _mapper.Map<AuthorForPresentationDTO>(authorCreated));
+        }
+
+        [HttpGet("{Id}", Name = "GetAuthor")]
+        public async Task<ActionResult<AuthorForPresentationDTO>> Get(int id)
+        {
+            Author author = await _service.GetAsync(id);
+
+            if (author == null) return NotFound();
+
+            return _mapper.Map<AuthorForPresentationDTO>(author);
+        }
+
+        [HttpPut("{Id}", Name = "UpdateAuthor")]
+        public async Task<IActionResult> UpdateAuthor(int id, [FromBody] AuthorForCreationDTO author)
+        {
+            if (!(await _service.ExistsAsync(id))) return NotFound();
+
+            Author authorToUpdate = _mapper.Map<Author>(author);
+            authorToUpdate.Id = id;
+
+            await _service.UpdateAsync(authorToUpdate);
+
+            return NoContent();
+        }
+    }
+}
