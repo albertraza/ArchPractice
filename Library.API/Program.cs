3d30209e8203e4144097b42eff6a734734eb0199
albert.ramirez <albert.ramirez@aloe.com.do>
2021-12-22 15:38:41 -0400

    change model state errors base implementation to be based on the rcf standard

--- Library.API/Program.cs
+++ Library.API/Program.cs
@@ -1,10 +1,55 @@
 using Library.Services.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(setupAction =>
+{
+    setupAction.ReturnHttpNotAcceptable = true;
+}).AddXmlDataContractSerializerFormatters()
+   .ConfigureApiBehaviorOptions(setupAction =>
+   {
+       setupAction.InvalidModelStateResponseFactory = context =>
+       {
+           // create a problem details object
+           ProblemDetailsFactory problemDetailsObject = context.HttpContext.RequestServices
+            .GetRequiredService<ProblemDetailsFactory>();
+
+           var problemDetails = problemDetailsObject.CreateValidationProblemDetails(context.HttpContext, context.ModelState);
+           problemDetails.Detail = "See the errors field for details.";
+           problemDetails.Instance = context.HttpContext.Request.Path;
+
+           // find out which status code to use.
+           var actionExecutingContext = context as Microsoft.AspNetCore.Mvc.Filters.ActionExecutingContext;
+
+           // if there are model state errors & all arguments were correctly fund/parsed we're dealing with validation errors.
+           if ((context.ModelState.ErrorCount > 0) && (
+            actionExecutingContext?.ActionArguments.Count == context.ActionDescriptor.Parameters.Count
+           ))
+           {
+               problemDetails.Type = "https://albertraza.com/ejemplo";
+               problemDetails.Status = StatusCodes.Status422UnprocessableEntity;
+               problemDetails.Title = "One or more validation errors occurred.";
+
+               return new UnprocessableEntityObjectResult(problemDetails)
+               {
+                   ContentTypes = { "application/problem+json" }
+               };
+           }
+
+           // if one of the arguments wasn't correctly found / couldn't be parsed.
+           // We're dealing with null/unparseable input.
+           problemDetails.Status = StatusCodes.Status400BadRequest;
+           problemDetails.Title = "One or more errors on input occurred.";
+           return new BadRequestObjectResult(problemDetails)
+           {
+               ContentTypes = { "application/problem+json" }
+           };
+       };
+   });
 
 builder.Services.ConfigureServices(builder.Configuration);
 
