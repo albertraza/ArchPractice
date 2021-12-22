658647336e6ca5cd7f6713ef14aa54a558d8087e
albert.ramirez <albert.ramirez@aloe.com.do>
2021-12-22 11:11:50 -0400

    change save changes implementation to run the logging logic before saving

--- Library.Data/ApplicationDbContext.cs
+++ Library.Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Library.Data.Extentions;
+using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
 namespace Library.Data
@@ -15,6 +16,30 @@ namespace Library.Data
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public override int SaveChanges()
+        {
+            this.DetectChanges();
+            return base.SaveChanges();
+        }
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            this.DetectChanges();
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+        }
+
+        public override async Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            await this.DetectChangesAsync();
+            return await base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            await this.DetectChangesAsync();
+            return await base.SaveChangesAsync(cancellationToken);
         }
     }
 }
