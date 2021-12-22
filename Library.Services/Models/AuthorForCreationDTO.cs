e05e4094f3fc14367565afefee9c86638cfff935
albert.ramirez <albert.ramirez@aloe.com.do>
2021-12-22 15:39:05 -0400

    add missing dto validation

--- Library.Services/Models/AuthorForCreationDTO.cs
+++ Library.Services/Models/AuthorForCreationDTO.cs
@@ -1,16 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Library.Services.ValidationAtributes;
+using System.ComponentModel.DataAnnotations;
 
 namespace Library.Services.Models
 {
     public class AuthorForCreationDTO
     {
         [Required]
+        [MaxLength(50)]
         public string FirstName { get; set; }
 
         [Required]
+        [MaxLength(50)]
         public string LastName { get; set; }
 
         [Required]
+        [DateNotGreaterThanToday(ErrorMessage = "The date born is invalid.")]
         public DateTime DateBorn { get; set; }
     }
 }
